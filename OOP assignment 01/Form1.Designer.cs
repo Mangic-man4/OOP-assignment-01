d780783bb044dc5518ff62163c219c5d6be5aa67
Mangic-man4 <Alexanderwillfor@gmail.com>
2023-09-27 16:05:31 +0300

    lol

--- OOP assignment 01/Form1.Designer.cs
+++ OOP assignment 01/Form1.Designer.cs
@@ -39,13 +39,21 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(302, 66);
+            this.button1.Location = new System.Drawing.Point(302, 88);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 44);
+            this.button1.Size = new System.Drawing.Size(160, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Make greeting";
             this.button1.UseVisualStyleBackColor = true;
@@ -72,79 +80,169 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(43, 154);
+            this.textBox2.Location = new System.Drawing.Point(43, 155);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Operand 1";
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(175, 153);
+            this.textBox3.Location = new System.Drawing.Point(152, 155);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 22);
             this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "Operand 2";
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(315, 154);
+            this.button2.Location = new System.Drawing.Point(302, 155);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(23, 23);
             this.button2.TabIndex = 6;
             this.button2.Text = "*";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(344, 154);
+            this.button3.Location = new System.Drawing.Point(331, 155);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(23, 23);
             this.button3.TabIndex = 7;
             this.button3.Text = "+";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(373, 154);
+            this.button4.Location = new System.Drawing.Point(360, 155);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(23, 23);
             this.button4.TabIndex = 8;
             this.button4.Text = "-";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(402, 154);
+            this.button5.Location = new System.Drawing.Point(389, 155);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(23, 23);
             this.button5.TabIndex = 9;
             this.button5.Text = "/";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(431, 154);
+            this.button6.Location = new System.Drawing.Point(418, 155);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(23, 23);
             this.button6.TabIndex = 10;
             this.button6.Text = "%";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(589, 154);
+            this.label2.Location = new System.Drawing.Point(586, 162);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 11;
             this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(43, 223);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(209, 22);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.Text = "Please enter your date of birth";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(43, 294);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(209, 22);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.Text = "Please enter a sentence";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(302, 221);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(160, 30);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Calculate days";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(308, 294);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(154, 31);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "Count characters";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(308, 357);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(154, 27);
+            this.button9.TabIndex = 16;
+            this.button9.Text = "True/false";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(586, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(586, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "label4";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(589, 357);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(120, 27);
+            this.panel1.TabIndex = 20;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -176,6 +274,14 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- OOP assignment 01/Form1.cs
+++ OOP assignment 01/Form1.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace OOP_assignment_01
 {
@@ -17,7 +22,6 @@ namespace OOP_assignment_01
         {
             InitializeComponent();
 
-            // Subscribe to events for each TextBox
             textBox1.GotFocus += TextBox_GotFocus;
             textBox1.LostFocus += TextBox_LostFocus;
 
@@ -27,37 +31,237 @@ namespace OOP_assignment_01
             textBox3.GotFocus += TextBox_GotFocus;
             textBox3.LostFocus += TextBox_LostFocus;
 
-            // Set the initial text for each TextBox
+            textBox4.GotFocus += TextBox_GotFocus;
+            textBox4.LostFocus += TextBox_LostFocus;
+
+            textBox5.GotFocus += TextBox_GotFocus;
+            textBox5.LostFocus += TextBox_LostFocus;
+
             textBox1.Text = "Please enter your name";
             textBox2.Text = "Operand 1";
             textBox3.Text = "Operand 2";
+            textBox4.Text = "Please enter your date of birth";
+            textBox5.Text = "Please enter a sentence";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string inputText = textBox1.Text;
+            string inputText = textBox1.Text.Trim();
 
-            label1.Text = "Hello " + inputText;
+            if (inputText.Equals("Please enter your name", StringComparison.OrdinalIgnoreCase))
+            {
+                label1.Text = "Hello";
+            }
+            else
+            {
+                label1.Text = "Hello " + inputText;
+            }
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string inputText1 = textBox2.Text;
+            string inputText2 = textBox3.Text;
 
+            float floatValue1;
+            float floatValue2;
+
+            if (float.TryParse(inputText1, out floatValue1) && float.TryParse(inputText2, out floatValue2))
+            {
+                float result = floatValue1 * floatValue2;
+
+                label2.Text = result.ToString();
+            }
+            else
+            {
+                label2.Text = "Invalid input. Please enter valid float values.";
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string inputText1 = textBox2.Text;
+            string inputText2 = textBox3.Text;
+
+            float floatValue1;
+            float floatValue2;
+
+            if (float.TryParse(inputText1, out floatValue1) && float.TryParse(inputText2, out floatValue2))
+            {
+                float result = floatValue1 + floatValue2;
+
+                label2.Text = result.ToString();
+            }
+            else
+            {
+                label2.Text = "Invalid input. Please enter valid float values.";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string inputText1 = textBox2.Text;
+            string inputText2 = textBox3.Text;
+
+            float floatValue1;
+            float floatValue2;
+
+            if (float.TryParse(inputText1, out floatValue1) && float.TryParse(inputText2, out floatValue2))
+            {
+                float result = floatValue1 - floatValue2;
+
+                label2.Text = result.ToString();
+            }
+            else
+            {
+                label2.Text = "Invalid input. Please enter valid float values.";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string inputText1 = textBox2.Text;
+            string inputText2 = textBox3.Text;
+
+            float floatValue1;
+            float floatValue2;
+
+            if (float.TryParse(inputText1, out floatValue1) && float.TryParse(inputText2, out floatValue2))
+            {
+                float result = floatValue1 / floatValue2;
+
+                label2.Text = result.ToString();
+            }
+            else
+            {
+                label2.Text = "Invalid input. Please enter valid float values.";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string inputText1 = textBox2.Text;
+            string inputText2 = textBox3.Text;
+
+            float floatValue1;
+            float floatValue2;
+
+            if (float.TryParse(inputText1, out floatValue1) && float.TryParse(inputText2, out floatValue2))
+            {
+                float result = floatValue1 % floatValue2;
+
+                label2.Text = result.ToString();
+            }
+            else
+            {
+                label2.Text = "Invalid input. Please enter valid float values.";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            string userInput = textBox4.Text;
+
+            string inputFormat = "yyyy-MM-dd";
+
+            DateTime parsedDate;
+            if (DateTime.TryParse(userInput, out parsedDate))
+            {
+                Console.WriteLine($"Parsed Date: {parsedDate}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid date input. Please enter a valid date.");
+            }
+
+            if (DateTime.TryParseExact(userInput, inputFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDate))
+            {
+
+                DateTime currentDate = DateTime.Today;
+                TimeSpan difference = currentDate - parsedDate;
+
+                int differenceInDays = (int)difference.TotalDays;
+
+                label3.Text = $"You are {differenceInDays} days old";
+            }
+            else
+            {
+                label3.Text = "Invalid date input. Please enter a valid date in the format yyyy-mm-dd.";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string text = textBox5.Text;
+            int characterCount = text.Length;
+            label4.Text = "Your sentence has " + characterCount.ToString() + " characters";
+        }
+
+        private bool isGreen = false;
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (isGreen)
+            {
+                // Change the color to Red
+                panel1.BackColor = Color.Red;
+                isGreen = false;
+            }
+            else
+            {
+                // Change the color to Green
+                panel1.BackColor = Color.Green;
+                isGreen = true;
+            }
+        }
+       
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
         {
 
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-            
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
         private void TextBox_GotFocus(object sender, EventArgs e)
         {
             TextBox textBox = (TextBox)sender;
 
-            if (textBox.Text == "Please enter your name")
+            if (textBox.Text == "Please enter your name" || textBox.Text == "Operand 1" || textBox.Text == "Operand 2" || 
+                textBox.Text == "Please enter your date of birth" || textBox.Text == "Please enter a sentence")
             {
-                textBox.Text = string.Empty; // Clear the description text
-                textBox.ForeColor = SystemColors.WindowText; // Restore the default text color
+                textBox.Text = string.Empty; 
+                textBox.ForeColor = SystemColors.WindowText; 
             }
         }
 
@@ -67,19 +271,29 @@ namespace OOP_assignment_01
 
             if (string.IsNullOrWhiteSpace(textBox.Text))
             {
-                textBox.Text = "Please enter your name"; // Restore the description text
-                textBox.ForeColor = SystemColors.GrayText; // Change text color to gray
+                if (textBox == textBox1)
+                {
+                    textBox.Text = "Please enter your name"; 
+                }
+                else if (textBox == textBox2)
+                {
+                    textBox.Text = "Operand 1";
+                }
+                else if (textBox == textBox3)
+                {
+                    textBox.Text = "Operand 2";
+                }
+                else if (textBox == textBox4)
+                {
+                    textBox.Text = "Please enter your date of birth";
+                }
+                else if (textBox == textBox5)
+                {
+                    textBox.Text = "Please enter a sentence";
+                }
+
+                textBox.ForeColor = SystemColors.GrayText;
             }
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
         }
     }
 }
